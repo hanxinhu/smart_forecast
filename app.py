5ff883325bb8885c7f3d47eb0057ea7463e7070a
hanxinhu <hanxinhu521@163.com>
2020-08-11 00:00:50 +0800

    chore: update config.json location

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask
 from flask import request, make_response, url_for, redirect, send_file, send_from_directory
 import json
 from flask_cors import CORS
+import os
+import sys
+root_path = os.path.split(os.path.realpath(sys.argv[0]))[0];
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-
 @app.route('/init')
 def hello_world():
-    f = open('./static/test.json')
+    f = open(root_path+'/config.json')
     string = f.readlines()
     s = "".join([i.strip() for i in string])
     return s
@@ -17,16 +19,7 @@ def hello_world():
 
 @app.route('/file')
 def get_file():
-    return send_file('img.png')
-
-
-@app.after_request
-def cors(environ):
-    environ = make_response(environ)
-    environ.headers['Access-Control-Allow-Origin'] = '*'
-    environ.headers['Access-Control-Allow-Method'] = '*'
-    environ.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
-    return environ
+    return send_file('static/img.png', mimetype='png')
 
 
 @app.route('/')
@@ -35,4 +28,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
